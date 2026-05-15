df3dd5f881bc9e609da396439d7674c36232aac6
김가연 <gyn38k@gmail.com>
2026-05-15 09:20:41 +0900

    upload

---
+++ matmatgotgot_front/src/pages/Main.jsx
@@ -0,0 +1,12 @@
+import "../App.css";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <h1>맛맛곳곳</h1>
+      <p>맛있는 음식과 아름다운 장소를 찾아보세요!</p>
+    </div>
+  );
+};
+
+export default Main;
