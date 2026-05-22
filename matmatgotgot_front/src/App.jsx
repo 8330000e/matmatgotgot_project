b504c33e52a42fbd4ab116cf52868b07a6ff2dd5
lshlsh98 <lsh980324@naver.com>
2026-05-22 16:15:33 +0900

    App.css 수정

--- matmatgotgot_front/src/App.jsx
+++ matmatgotgot_front/src/App.jsx
@@ -5,17 +5,23 @@ import MainCover from "./pages/MainCover";
 import Main from "./pages/Main";
 import Login from "./pages/Login";
 import Join from "./pages/Join";
+import RestaurantMain from "./pages/restaurant/RestaurantMain";
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<MainCover/>} />
-        <Route path="/main" element={<Main/>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Join />} />
-      </Routes>
-    </>
+    <div className="wrap">
+      {/* <Header /> */}
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<MainCover />} />
+          <Route path="/main" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Join />} />
+          <Route path="/restaurantmain" element={<RestaurantMain />} />
+        </Routes>
+      </div>
+      {/* <Footer /> */}
+    </div>
   );
 }
 
---
+++ matmatgotgot_front/src/components/restaurant/RestaurantItem.jsx
@@ -0,0 +1,9 @@
+const RestaurantItem = ({ rest }) => {
+  return (
+    <>
+      <h1>맛집 아이템2</h1>
+    </>
+  );
+};
+
+export default RestaurantItem;
