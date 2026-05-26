624e61d38604e78642bb171372707f3ae7896119
김가연 <gyn38k@gmail.com>
2026-05-26 17:42:10 +0900

    도메인 구조 수정

---
+++ matmatgotgot_back/src/main/java/com/twotwo/matmatgotgot/domain/member/entity/LoginMember.java
@@ -0,0 +1,25 @@
+package com.twotwo.matmatgotgot.domain.member.entity;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class LoginMember {
+	private Long memberNo;
+    private String memberId;
+    private String memberPw;
+    private String memberName;
+    private String memberEmail;
+    private String memberNickname;
+    private String memberThumb;
+    private int memberStatus;
+    private LocalDateTime enrollDate;
+    private boolean admin;
+    private String socialLogin;
+}
+
