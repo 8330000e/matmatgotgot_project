0600c8c484dd0750df2a5954638005c0e06a991d
김가연 <gyn38k@gmail.com>
2026-05-26 09:13:45 +0900

    헤더 콘솔 잠시 주석

--- matmatgotgot_front/src/App.jsx
+++ matmatgotgot_front/src/App.jsx
@@ -5,23 +5,23 @@ import Main from "./pages/Main";
 import RestaurantMain from "./pages/restaurant/RestaurantMain";
 import Login from "./pages/member/Login.jsx";
 import Join from "./pages/member/Join.jsx";
-import {useEffect} from "react";
+import { useEffect } from "react";
 import axios from "axios";
 import { useAuthStore } from "./store/useAuthStore.js";
 
 function App() {
   //관리자 페이지 로직
-  
+
   const token = useAuthStore((state) => state.token);
-  useEffect(()=>{
+  useEffect(() => {
     if (token) {
       axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-      console.log("새로고침 후 Axios 헤더 세팅 완료", token);
+      // console.log("새로고침 후 Axios 헤더 세팅 완료", token);
     } else {
       delete axios.defaults.headers.common["Authorization"];
-      console.log("Axios Authorization 헤더 제거 완료");
+      // console.log("Axios Authorization 헤더 제거 완료");
     }
-  },[token]);
+  }, [token]);
 
   return (
     <div className="wrap">
