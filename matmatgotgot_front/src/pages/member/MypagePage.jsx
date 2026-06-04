8a5eed636c551456f832703d0b3c5e53fb6f46ad
김가연 <gyn38k@gmail.com>
2026-06-04 12:19:28 +0900

    마이페이지

--- matmatgotgot_front/src/pages/member/MypagePage.jsx
+++ matmatgotgot_front/src/pages/member/MypagePage.jsx
@@ -1,8 +1,10 @@
 import styles from "./MypagePage.module.css";
 import { useAuthStore } from '../../store/useAuthStore';
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 
-const MypagePage = () => {
+export const MypagePage = () => {
+    const location = useLocation();
+    const path = location.pathname.substring(8);
   const logout = useAuthStore((state) => state.logout);
   return (
     <div className="mypage">
@@ -15,40 +17,120 @@ const MypagePage = () => {
                 </div>
                 <div>
                     <ul className={styles.menubar_wrap}>
-                        <li>
-                            <Link to="/mypage/myinfo">내 정보</Link>
-                        </li>
-                        <li>
-                            <Link to="/mypage/myinfo">내 리뷰</Link>
-                        </li>
-                        <li>
-                            <Link to="/mypage/myinfo">찜한 맛집 목록</Link>
-                        </li>
-                        <li>
-                            <Link to="/mypage/myinfo">맛집 여행 목록</Link>
-                        </li>
-                        <li>
-                            <Link to="/mypage/myinfo">좋아요 게시글</Link>
-                        </li>
-                        <li>
-                            <Link to="/mypage/myinfo">내 게시글</Link>
-                        </li>
-                        <li>
-                            <Link to="/mypage/myinfo">신고 게시글</Link>
-                        </li>
-                        <li>
-                            <Link to="/mypage/myinfo">1:1 문의 내역</Link>
-                        </li>
+                        <Link to="/mypage/myinfo">
+                            <li className={path === "myinfo" ? styles.sidebar_active : styles.sidebar_default}>
+                            내 정보
+                            </li>
+                        </Link>
+                        <Link to="/mypage/myreview">
+                            <li className={path === "myreview" ? styles.sidebar_active : styles.sidebar_default}>
+                            내 리뷰
+                            </li>
+                        </Link>
+                        <Link to="/mypage/zzim">
+                            <li className={path === "zzim" ? styles.sidebar_active : styles.sidebar_default}>
+                            찜한 맛집 목록
+                            </li>
+                        </Link>
+                        <Link to="/mypage/matzip">
+                            <li className={path === "matzip" ? styles.sidebar_active : styles.sidebar_default}>
+                           맛집 여행 목록
+                            </li>
+                        </Link>
+                        <Link to="/mypage/likeposts">
+                            <li className={path === "likeposts" ? styles.sidebar_active : styles.sidebar_default}>
+                            좋아요 게시글
+                            </li>
+                        </Link>
+                        <Link to="/mypage/myposts">
+                            <li className={path === "myposts" ? styles.sidebar_active : styles.sidebar_default}>
+                            내 게시글
+                            </li>
+                        </Link>
+                        <Link to="/mypage/reportposts">
+                            <li className={path === "reportposts" ? styles.sidebar_active : styles.sidebar_default}>
+                            신고 게시글
+                            </li>
+                        </Link>
+                        <Link to="/mypage/myask">
+                            <li className={path === "myask" ? styles.sidebar_active : styles.sidebar_default}>
+                            1:1 문의 내역
+                            </li>
+                        </Link>
                     </ul>
-
                 </div>
             </div>
             <div className={styles.content_wrap}>
-
+                {path === "myinfo" && <Myinfo />}
+                {path === "myreview" && <Myreview />}
+                {path === "zzim" && <Zzim />}
+                {path === "matzip" && <Matzip />}
+                {path === "likeposts" && <Likeposts />}
+                {path === "myposts" && <Myposts />}
+                {path === "reportposts" && <Reportposts />}
+                {path === "myask" && <Myask />}
             </div>
         </div>
     </div>
   );
 };
 
-export default MypagePage;
+export const Myinfo = () => {
+    return (<>
+        <div className={styles.content_menu_wrap}>
+            <div>키에에</div>
+        </div>
+    </>);
+};
+
+export const Myreview = () => {
+    return (<>
+        <div className={styles.content_menu_wrap}>
+            <div>키에에</div>
+        </div>
+    </>);};
+
+export const Zzim = () => {
+    return (<>
+        <div className={styles.content_menu_wrap}>
+            <div>우우우</div>
+        </div>
+    </>);};
+
+export const Matzip = () => {
+    return (<>
+        <div className={styles.content_menu_wrap}>
+
+        </div>
+    </>);};
+
+export const Likeposts = () => {
+    return (<>
+        <div className={styles.content_menu_wrap}>
+
+        </div>
+    </>);};
+
+export const Myposts = () => {
+    return (<>
+        <div className={styles.content_menu_wrap}>
+
+        </div>
+    </>);};
+
+export const Reportposts = () => {
+    return (<>
+        <div className={styles.content_menu_wrap}>
+
+        </div>
+    </>);};
+
+export const Myask = () => {
+    return (<>
+        <div className={styles.content_menu_wrap}>
+
+        </div>
+    </>);};
+
+
+export default { MypagePage, Myinfo, Myreview, Zzim, Matzip, Likeposts, Myposts, Reportposts, Myask };
