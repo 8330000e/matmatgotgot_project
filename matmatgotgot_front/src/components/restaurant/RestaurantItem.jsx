b89f3e4e697e37adac2319fba751a5eea5956d7e
lshlsh98 <lsh980324@naver.com>
2026-06-08 04:40:35 +0900

    restaurantItem - 좋아요 구분

--- matmatgotgot_front/src/components/restaurant/RestaurantItem.jsx
+++ matmatgotgot_front/src/components/restaurant/RestaurantItem.jsx
@@ -1,6 +1,7 @@
 import styles from "./RestaurantItem.module.css";
 import ImageNotSupportedIcon from "@mui/icons-material/ImageNotSupported";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import { useNavigate } from "react-router-dom";
 
 const RestaurantItem = ({ rest }) => {
@@ -17,7 +18,11 @@ const RestaurantItem = ({ rest }) => {
         <div className={styles.name_like}>
           <div className={styles.rest_name}>{rest.restName}</div>
           <div className={styles.like}>
-            <FavoriteIcon className={styles.favorite_icon} />
+            {rest.isLike ? (
+              <FavoriteIcon className={styles.favorite_icon} />
+            ) : (
+              <FavoriteBorderIcon className={styles.favorite_icon} />
+            )}
           </div>
         </div>
         <div className={styles.rest_img}>
@@ -34,7 +39,7 @@ const RestaurantItem = ({ rest }) => {
         <div className={styles.review_count}>
           리뷰수 {rest.reviewTotalCount}
         </div>
-        <div className={styles.star}>★ {rest?.ratingAvg ?? 5.0}</div>
+        <div className={styles.star}>★ {rest?.ratingAvg ?? 0}</div>
       </div>
     </div>
   );
