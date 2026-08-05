01a0abd97ae828e5b74a663e562183adce06245d
김가연 <gyn38k@gmail.com>
2026-08-05 13:02:15 +0900

    모이치도

--- matmatgotgot_back/src/main/java/com/twotwo/matmatgotgot/domain/main/service/MainService.java
+++ matmatgotgot_back/src/main/java/com/twotwo/matmatgotgot/domain/main/service/MainService.java
@@ -3,11 +3,15 @@ package com.twotwo.matmatgotgot.domain.main.service;
 import com.twotwo.matmatgotgot.domain.main.dto.response.MainBestReviewDTO;
 import com.twotwo.matmatgotgot.domain.main.dto.response.MainBestTourDTO;
 import com.twotwo.matmatgotgot.domain.main.mapper.MainMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class MainService {
     private final MainMapper mainMapper;
 
