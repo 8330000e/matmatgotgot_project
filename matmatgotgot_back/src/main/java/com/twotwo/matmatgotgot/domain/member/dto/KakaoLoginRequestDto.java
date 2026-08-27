92d92d66649439f49bc571951f4969063b7b20b2
김가연 <gyn38k@gmail.com>
2026-08-27 09:46:05 +0900

    m

--- matmatgotgot_back/src/main/java/com/twotwo/matmatgotgot/domain/member/dto/KakaoLoginRequestDto.java
+++ matmatgotgot_back/src/main/java/com/twotwo/matmatgotgot/domain/member/dto/KakaoLoginRequestDto.java
@@ -1,6 +1,5 @@
 package com.twotwo.matmatgotgot.domain.member.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,17 +8,11 @@ import lombok.ToString;
 
 @Getter
 @Setter
-@ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class KakaoLoginRequestDto {
-
-    @JsonProperty("memberEmail")
     private String memberEmail;
-
-    @JsonProperty("memberNickname")
     private String memberNickname;
-
-    @JsonProperty("memberThumb")
     private String memberThumb;
 }
