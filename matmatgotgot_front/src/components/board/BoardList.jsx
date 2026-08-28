6374b15cac7c774bf6c841e04b7e4d4428718356
김가연 <gyn38k@gmail.com>
2026-08-28 10:18:33 +0900

    m

--- matmatgotgot_front/src/components/board/BoardList.jsx
+++ matmatgotgot_front/src/components/board/BoardList.jsx
@@ -3,7 +3,7 @@ import defaultImage from '../../assets/board/image.png';
 import defaultImg from '../../assets/board/user.png';
 import { useNavigate } from "react-router-dom";
 import { useAuthStore } from "../../store/useAuthStore";
-import { getProfileThumb } from "@/utils/imageUtil";
+import { getProfileThumb } from "../util/getProfileThumb";
 
 const BoardList = ({ boardList }) => {
   return (
