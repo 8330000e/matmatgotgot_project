813a1df656b3fcda93198a94cf949ef70a170919
김가연 <gyn38k@gmail.com>
2026-09-04 16:01:51 +0900

    m

--- matmatgotgot_back/src/main/java/com/twotwo/matmatgotgot/domain/member/service/MemberService.java
+++ matmatgotgot_back/src/main/java/com/twotwo/matmatgotgot/domain/member/service/MemberService.java
@@ -203,7 +203,7 @@ public class MemberService {
         Natives nativeInfo = memberMapper.getNative(memberId);
         Natives nativeDate = memberMapper.getNativeDate(memberId);
         if (nativeDate == null) {
-            return nativeInfo;
+            return null; // 또는 null 응답 객체 처리
         }else {
             nativeInfo.setNativeStatus(nativeDate.getNativeStatus());
         }
