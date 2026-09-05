581e4980ebf44fd0e1f7e033f2a03a1efa1d73fb
김가연 <gyn38k@gmail.com>
2026-09-05 10:08:26 +0900

    m

--- matmatgotgot_front/src/utils/imageUtil.js
+++ matmatgotgot_front/src/utils/imageUtil.js
@@ -1,16 +1,19 @@
 import defaultImg from "../assets/img/defaultImg.svg";
 
-// 기존 작성했던 프로필 이미지 처리 함수
 export const getProfileImageUrl = (thumb, defaultImg) => {
+  // ⭕ thumb가 null, undefined, 빈 문자열이거나 string 타입이 아닌 경우 즉시 defaultImg 반환
   if (!thumb || typeof thumb !== "string") {
     return defaultImg || null;
   }
+
+  // 소셜 로그인 등 HTTP/HTTPS 외부 경로
   if (thumb.startsWith("http://") || thumb.startsWith("https://")) {
     return thumb;
   }
+
+  // 문자열임이 보장된 상태에서 안전하게 slice 실행
   const cleanPath = thumb.startsWith("/") ? thumb.slice(1) : thumb;
   return `https://d2lg74d5mqmhqe.cloudfront.net/app/upload/web/matgot/menu/${cleanPath}`;
 };
 
-// ⭕ 게시판 컴포넌트들에서 참조 중인 getProfileThumb 추가 (동일 로직 바인딩)
 export const getProfileThumb = getProfileImageUrl;
