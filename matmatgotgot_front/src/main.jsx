cc2915d34a8a9d9da566eb95528a0533fc8456b6
김가연 <gyn38k@gmail.com>
2026-09-05 17:48:39 +0900

    m

--- matmatgotgot_front/src/main.jsx
+++ matmatgotgot_front/src/main.jsx
@@ -8,50 +8,50 @@ import App from "./App.jsx";
 import axios from 'axios'
 import { useAuthStore } from './store/useAuthStore'
 
-axios.interceptors.request.use(
-  (config) => {
-    // Zustand 스토어나 localStorage에서 토큰 가져오기
-    const token = useAuthStore.getState().token || localStorage.getItem("token");
+// axios.interceptors.request.use(
+//   (config) => {
+//     // Zustand 스토어나 localStorage에서 토큰 가져오기
+//     const token = useAuthStore.getState().token || localStorage.getItem("token");
     
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
 
-axios.interceptors.response.use(
-  (response) => response, // 성공 응답은 그대로 리턴
-  (error) => {
-    const originalRequest = error.config;
+// axios.interceptors.response.use(
+//   (response) => response, // 성공 응답은 그대로 리턴
+//   (error) => {
+//     const originalRequest = error.config;
 
-    // 401 Unauthorized 또는 403 Forbidden 에러 감지
-    if (error.response && (error.response.status === 401 || error.response.status === 403)) {
-      !originalRequest.url.includes("/members/login");
-      // 중복 알림/리다이렉트 방지
-      if (!originalRequest._retry) {
-        originalRequest._retry = true;
+//     // 401 Unauthorized 또는 403 Forbidden 에러 감지
+//     if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+//       !originalRequest.url.includes("/members/login");
+//       // 중복 알림/리다이렉트 방지
+//       if (!originalRequest._retry) {
+//         originalRequest._retry = true;
 
-        console.warn("JWT 토큰이 만료되었거나 유효하지 않습니다.");
+//         console.warn("JWT 토큰이 만료되었거나 유효하지 않습니다.");
 
-        // 스토어 상태 및 Storage 초기화
-        const logout = useAuthStore.getState().logout;
-        if (logout) {
-          logout();
-        } else {
-          localStorage.removeItem("token");
-          localStorage.removeItem("user");
-        }
+//         // 스토어 상태 및 Storage 초기화
+//         const logout = useAuthStore.getState().logout;
+//         if (logout) {
+//           logout();
+//         } else {
+//           localStorage.removeItem("token");
+//           localStorage.removeItem("user");
+//         }
 
-        alert("세션이 만료되었습니다. 다시 로그인해 주세요.");
-        window.location.href = "/"; // 메인/로그인 페이지로 이동
-      }
-    }
+//         alert("세션이 만료되었습니다. 다시 로그인해 주세요.");
+//         window.location.href = "/"; // 메인/로그인 페이지로 이동
+//       }
+//     }
 
-    return Promise.reject(error);
-  }
-);
+//     return Promise.reject(error);
+//   }
+// );
 
 console.log(import.meta.env.VITE_GOOGLE_CLIENT_ID);
 
