a62508d8bf065adc3a7d56942f7606fad7a3951b
김가연 <gyn38k@gmail.com>
2026-09-06 16:14:11 +0900

    m

--- matmatgotgot_front/src/main.jsx
+++ matmatgotgot_front/src/main.jsx
@@ -10,48 +10,48 @@ import { useAuthStore } from './store/useAuthStore'
 
 console.log("아이디: ", useAuthStore.getState().memberId, "\n토큰: ", useAuthStore.getState().token);
 
-axios.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    const originalRequest = error.config;
+// axios.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     const originalRequest = error.config;
 
-    // 401 / 403 에러 처리
-    // 단, 로그인 요청은 제외
-    if (
-      error.response &&
-      (error.response.status === 401 || error.response.status === 403) &&
-      !originalRequest.url.includes("/members/login")
-    ) {
-      console.log(
-        "아이디: ",
-        useAuthStore.getState().memberId,
-        "\n토큰: ",
-        useAuthStore.getState().token
-      );
+//     // 401 / 403 에러 처리
+//     // 단, 로그인 요청은 제외
+//     if (
+//       error.response &&
+//       (error.response.status === 401 || error.response.status === 403) &&
+//       !originalRequest.url.includes("/members/login")
+//     ) {
+//       console.log(
+//         "아이디: ",
+//         useAuthStore.getState().memberId,
+//         "\n토큰: ",
+//         useAuthStore.getState().token
+//       );
 
-      // 중복 알림/리다이렉트 방지
-      if (!originalRequest._retry) {
-        originalRequest._retry = true;
+//       // 중복 알림/리다이렉트 방지
+//       if (!originalRequest._retry) {
+//         originalRequest._retry = true;
 
-        console.warn("JWT 토큰이 만료되었거나 유효하지 않습니다.");
+//         console.warn("JWT 토큰이 만료되었거나 유효하지 않습니다.");
 
-        const logout = useAuthStore.getState().logout;
+//         const logout = useAuthStore.getState().logout;
 
-        if (logout) {
-          logout();
-        } else {
-          localStorage.removeItem("token");
-          localStorage.removeItem("user");
-        }
+//         if (logout) {
+//           logout();
+//         } else {
+//           localStorage.removeItem("token");
+//           localStorage.removeItem("user");
+//         }
 
-        alert("세션이 만료되었습니다. 다시 로그인해 주세요.");
-        window.location.href = "/";
-      }
-    }
+//         alert("세션이 만료되었습니다. 다시 로그인해 주세요.");
+//         window.location.href = "/";
+//       }
+//     }
 
-    return Promise.reject(error);
-  }
-);
+//     return Promise.reject(error);
+//   }
+// );
 
 console.log(import.meta.env.VITE_GOOGLE_CLIENT_ID);
 
