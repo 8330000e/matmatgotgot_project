3692e0d41ad796fe4936c2fc11fa2bc7dc13a9ca
김가연 <gyn38k@gmail.com>
2026-09-10 11:34:43 +0900

    m

--- matmatgotgot_back/src/main/java/com/twotwo/matmatgotgot/domain/board/mapper/BoardMapper.java
+++ matmatgotgot_back/src/main/java/com/twotwo/matmatgotgot/domain/board/mapper/BoardMapper.java
@@ -86,7 +86,5 @@ public interface BoardMapper {
 
     List<Review> selectMyReviewList(@Param("item") ListItem item, @Param("memberId") String memberId);
 
-    int selectMyReviewCount(@Param("item") ListItem item);
-
     Integer selectMyReviewCount(ListItem request, String memberId);
 }
