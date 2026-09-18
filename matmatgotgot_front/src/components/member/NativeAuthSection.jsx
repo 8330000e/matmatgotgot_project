d355cd8dcdcbfb99315469b5532b18a83c6e9e0c
김가연 <gyn38k@gmail.com>
2026-09-18 17:02:45 +0900

    m

--- matmatgotgot_front/src/components/member/NativeAuthSection.jsx
+++ matmatgotgot_front/src/components/member/NativeAuthSection.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import styles from './NativeAuthSection.module.css';
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 
 function NativeAuthSection({check}) {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -12,9 +13,14 @@ function NativeAuthSection({check}) {
       {isModalOpen && ReactDOM.createPortal(
       <div className={styles.modal_overlay}>
         <div className={styles.modal_content}>
-          <h2>동네 인증</h2>
-          <p>현재 위치 정보를 기반으로 인증을 진행합니다.</p>
-          <button onClick={() => setIsModalOpen(false)}>닫기</button>
+          <div>
+            <h2>현지인 인증</h2>
+            <button className={styles.xbutton} onClick={() => setIsModalOpen(false)}>{CloseOutlinedIcon}</button>
+          </div>
+          <div>
+            <p>현재 위치 정보를 기반으로 인증을 진행합니다.</p>
+          </div>
+          <button>인증하기</button>
         </div>
       </div>,
       document.body // 2. 타겟을 document.body로 지정!
