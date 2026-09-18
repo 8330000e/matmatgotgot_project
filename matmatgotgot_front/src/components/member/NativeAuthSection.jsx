b339613008f0c9bb00bbf342c6a89c7f07bda83e
김가연 <gyn38k@gmail.com>
2026-09-18 17:11:50 +0900

    re

--- matmatgotgot_front/src/components/member/NativeAuthSection.jsx
+++ matmatgotgot_front/src/components/member/NativeAuthSection.jsx
@@ -14,7 +14,7 @@ function NativeAuthSection({check}) {
       <div className={styles.modal_overlay}>
         <div className={styles.modal_content}>
             <h2>현지인 인증</h2>
-            <button className={styles.xbutton} onClick={() => setIsModalOpen(false)}>{CloseOutlinedIcon}</button>
+            <button className={styles.xbutton} onClick={() => setIsModalOpen(false)}><CloseOutlinedIcon/></button>
           <div>
             <p>현재 위치 정보를 기반으로 인증을 진행합니다.</p>
           </div>
