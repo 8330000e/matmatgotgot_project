f98c08ec29d86c54cf57255033aaa8a59b1a5e24
김가연 <gyn38k@gmail.com>
2026-09-19 19:54:17 +0900

    m

--- matmatgotgot_front/src/components/member/NativeAuthSection.jsx
+++ matmatgotgot_front/src/components/member/NativeAuthSection.jsx
@@ -86,35 +86,35 @@ function NativeAuthSection({check, memberInfo, native, setMemberInfo}) {
                     <li>26.12.11</li>
                   </ul>
                   <ul className={styles.ullist}>
-                    <li>2</li>
+                    <li>3</li>
                     <li>강서구</li>
                     <li>모퉁이김밥</li>
                     <li>김밥은 무조건...</li>
                     <li>26.12.11</li>
                   </ul>
                   <ul className={styles.ullist}>
-                    <li>3</li>
+                    <li>4</li>
                     <li>강서구</li>
                     <li>옆집떡볶이</li>
                     <li>떡볶이의 대가...</li>
                     <li>27.02.26</li>
                   </ul>
                   <ul className={styles.ullist}>
-                    <li>4</li>
+                    <li>5</li>
                     <li>강서구</li>
                     <li>된장찌개짱</li>
                     <li>제가 또 된장...</li>
                     <li>27.03.22</li>
                   </ul>
                   <ul className={styles.ullist}>
-                    <li>5</li>
+                    <li>6</li>
                     <li>강서구</li>
                     <li>빵지순례</li>
                     <li>미쳤습니다요즘...</li>
                     <li>27.03.31</li>
                   </ul>
                   <ul className={styles.ullist}>
-                    <li>6</li>
+                    <li>7</li>
                     <li>강서구</li>
                     <li>동양찻집</li>
                     <li>마음의 치유를...</li>
